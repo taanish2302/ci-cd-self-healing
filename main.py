a1d9012bef0805e044f07ce56501333152fba286
taanish2302 <75430328+taanish2302@users.noreply.github.com>
2026-04-16 10:35:08 +0530

    Implement error handling for numpy import
    Add error handling for numpy import and log errors

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-
-print("Hello CI/CD
+try:
+    import numpy  # will fail if not installed
+    print("All good")
+except Exception as e:
+    with open("error.log", "w") as f:
+        f.write(str(e))
+    raise e
