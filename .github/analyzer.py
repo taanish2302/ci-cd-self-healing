54dc48a5800f63b39c9cfea7ceb8c06714a2902e
taanish2302 <75430328+taanish2302@users.noreply.github.com>
2026-04-16 10:41:25 +0530

    Update analyzer.py

--- .github/analyzer.py
+++ .github/analyzer.py
@@ -1,30 +1,5 @@
-import json
-import os
-
-# load dataset
-with open("data.json") as f:
-    data = json.load(f)
-
 def find_fix(error):
     for d in data:
-        if d["error_log"] in error:
+        if d["error_log"].lower() in error.lower():
             return d["fix"]
     return None
-
-# read error log automatically
-if os.path.exists("error.log"):
-    with open("error.log") as f:
-        error = f.read()
-else:
-    print("No error log found")
-    exit()
-
-print("Detected Error:", error)
-
-fix = find_fix(error)
-
-if fix:
-    print("Applying Fix:", fix)
-    os.system(fix)  # runs pip install etc
-else:
-    print("No fix found")
